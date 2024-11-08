88f0b30165181bd81d2d81797d62d6980d264716
nhutnamlv <namtran.040104n@gmail.com>
2024-11-08 22:54:05 +0700

    lab2_8_11

---
+++ nhutnamlv-6351071047_LTWEB_K63/6351071047_LTWEB_K63/Controllers/dbQLBansachDataContext.cs
@@ -0,0 +1,7 @@
+﻿namespace _6351071047_LTWEB_K63.Controllers
+{
+    internal class dbQLBansachDataContext
+    {
+        public object SACH { get; internal set; }
+    }
+}
---
+++ nhutnamlv-6351071047_LTWEB_K63/6351071047_LTWEB_K63/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_6351071047_LTWEB_K63.MvcApplication" Language="C#" %>
