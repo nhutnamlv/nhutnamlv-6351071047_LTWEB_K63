00504715235d3cffef02921dfc5418e203da37fa
nhutnamlv <namtran.040104n@gmail.com>
2024-11-08 17:49:47 +0700

    Đưa thư mục bị loại trừ trở lại

---
+++ 6351071047_LTWEB_K63/Controllers/NguoidungController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _6351071047_LTWEB_K63.Controllers
+{
+    public class NguoidungController : Controller
+    {
+        // GET: Nguoidung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+    }
+}
