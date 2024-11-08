0c8361446f233f708b5ed23ac7ab92a5e2c8d995
nhutnamlv <namtran.040104n@gmail.com>
2024-11-08 22:43:03 +0700

    lab2

---
+++ nhutnamlv-6351071047_LTWEB_K63-master/6351071047_LTWEB_K63/Controllers/NguoidungController.cs
@@ -0,0 +1,123 @@
+﻿using _6351071047_LTWEB_K63.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _6351071047_LTWEB_K63.Controllers
+{
+    
+    public class NguoidungController : Controller
+    {
+        // GET: Nguoidung
+        QLBANSACHEntities db = new QLBANSACHEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+          //  QLBANSACHEntities1 db = new QLBANSACHEntities1();
+            QLBANSACHEntities db = new QLBANSACHEntities();
+
+        // Gán các giá trị người dùng nhập liệu cho các biến
+        var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Matkhaunhaplai"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(matkhaunhaplai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email cannot be left blank";
+            }
+            if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Error6"] = "Must enter phone number";
+            }
+            else
+            {
+                kh.HoTen = hoten;
+
+                kh.Taikhoan = tendn;
+
+                kh.Matkhau = matkhau;
+
+                kh.Email = email;
+
+                kh.DiachiKH = diachi;
+
+                kh.DienthoaiKH = dienthoai;
+
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+
+                db.KHACHHANGs.Add(kh);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return this.Dangky();
+        }
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến
+            var tendn = collection["TenDN"];
+            var matkhau = collection["MatKhau"];
+
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                // Gán giá trị cho đối tượng được tạo mới (kh)
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.Taikhoan == tendn && n.Matkhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+
+    }
+}
